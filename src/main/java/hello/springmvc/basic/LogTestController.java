5a6e7c2a135da63c9dc7f059a997eccbfadc2e91
jaemoon <woans5970@naver.com>
2023-12-28 23:11:24 +0900

    section 6: Logging

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,26 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+        log.trace("trace log={}", name);
+//        log.trace("trace log=" + name); // 더하기 연산이 있어서 쓸데없는 자원을 사용함 그래서 이렇게 쓰면 안됨!!!
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+        return "ok";
+    }
+}
